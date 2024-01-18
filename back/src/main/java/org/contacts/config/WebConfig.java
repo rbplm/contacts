4c1ab793c3115d223d71950a96b5bb670feef264
Kasutaja <roby.palumae@gmail.com>
2024-01-18 23:48:19 +0200

    c

--- back/src/main/java/org/contacts/config/WebConfig.java
+++ back/src/main/java/org/contacts/config/WebConfig.java
@@ -10,10 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOriginPatterns("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-
